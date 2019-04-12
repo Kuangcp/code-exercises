3450049628ef83a5ca133f4384c9f9c9293c1ac5
Kuangcp <kuangcp@aliyun.com>
2019-04-13 00:27:13 +0800

    +) log min(m,n)

--- java-exercises/src/test/java/com/github/kuangcp/leetcode/array/P0004_MedianOfTwoSortedArrays.java
+++ java-exercises/src/test/java/com/github/kuangcp/leetcode/array/P0004_MedianOfTwoSortedArrays.java
@@ -99,4 +99,50 @@ public class P0004_MedianOfTwoSortedArrays {
       }
     }
   }
+
+  /////////////////////
+
+  public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+    return 0;
+  }
+
+  ///////////////////
+
+  // https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2481/Share-my-O(log(min(mn))-solution-with-explanation
+  // log min(n,m)
+  public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
+    int len1 = nums1.length;
+    int len2 = nums2.length;
+
+    // make sure nums1 <= nums2
+    if (len1 > len2) {
+      return findMedianSortedArrays3(nums2, nums1);
+    }
+
+    int k = (len1 + len2 + 1) / 2;
+    int l = 0;
+    int r = len1;
+
+    //find the m1 so that nums1[m1] >= nums2[m2 - 1]
+    while (l < r) {
+      int m1 = l + (r - l) / 2;
+      int m2 = k - m1;
+      if (nums1[m1] < nums2[m2 - 1]) {
+        l = m1 + 1;
+      } else {
+        r = m1;
+      }
+    }
+
+    int m1 = l;
+    int m2 = k - l;
+    int c1 = Math.max(m1 <= 0 ? Integer.MIN_VALUE : nums1[m1 - 1],
+        m2 <= 0 ? Integer.MIN_VALUE : nums2[m2 - 1]);
+    if ((len1 + len2) % 2 == 1) {
+      return c1;
+    }
+    int c2 = Math.min(m1 >= len1 ? Integer.MAX_VALUE : nums1[m1],
+        m2 >= len2 ? Integer.MAX_VALUE : nums2[m2]);
+    return (c1 + c2) / 2.0;
+  }
 }
--- java-exercises/src/test/java/com/github/kuangcp/leetcode/array/P0004_MedianOfTwoSortedArraysTest.java
+++ java-exercises/src/test/java/com/github/kuangcp/leetcode/array/P0004_MedianOfTwoSortedArraysTest.java
@@ -3,6 +3,7 @@ package com.github.kuangcp.leetcode.array;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
+import java.util.function.BinaryOperator;
 import org.junit.Test;
 
 /**
@@ -13,9 +14,17 @@ public class P0004_MedianOfTwoSortedArraysTest {
   private P0004_MedianOfTwoSortedArrays target = new P0004_MedianOfTwoSortedArrays();
 
   @Test
-  public void test(){
-    assertThat(target.findMedianSortedArrays(new int[]{1,2}, new int[]{3,4}), equalTo(2.5));
-    assertThat(target.findMedianSortedArrays(new int[]{}, new int[]{3,4}), equalTo(3.5));
-    assertThat(target.findMedianSortedArrays(new int[]{1,3}, new int[]{2}), equalTo(2.0));
+  public void test() {
+    assertThat(target.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), equalTo(2.5));
+    assertThat(target.findMedianSortedArrays(new int[]{}, new int[]{3, 4}), equalTo(3.5));
+    assertThat(target.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), equalTo(2.0));
   }
+
+  @Test
+  public void test3() {
+    assertThat(target.findMedianSortedArrays3(new int[]{1, 2}, new int[]{3, 4}), equalTo(2.5));
+    assertThat(target.findMedianSortedArrays3(new int[]{}, new int[]{3, 4}), equalTo(3.5));
+    assertThat(target.findMedianSortedArrays3(new int[]{1, 3}, new int[]{2}), equalTo(2.0));
+  }
+
 }
